257eeaf83871a5002201409974020ca9549ab211
Sergey <sergeymanucharyan5@gmail.com>
2023-02-21 13:10:52 -0800

    String Homework

--- src/string_homework/Main.java
+++ src/string_homework/Main.java
@@ -3,23 +3,28 @@ package string_homework;
 public class Main {
     public static void main(String[] args) {
         Strings str = new Strings();
+        SecondStringHomework str1 = new SecondStringHomework();
 
-        System.out.println(str.concat("hello", " world"));
-        System.out.println(str.concat2(1, 2, " test code"));
+//        System.out.println(str.concat("hello", " world"));
+//        System.out.println(str.concat2(1, 2, " test code"));
+//
+//        str.newLine("Java Academic Program");//2
+//
+//        str.revers("Coffee");//3
+//
+//        System.out.println(str.countSubString("Nice to meet you!!", "e"));//4
+//
+//        System.out.println(str.toUppercase("java"));//5
+//
+//        System.out.println(str.removeSymbol("java academic program"));//7
+//        System.out.println(str.transferSymA("java academic program"));//8
+//
+//        str.getLongestWord("Hardships often prepare ordinary people for an extraordinary destiny");//9
+//
+//        str.symbolIndex("oop in java ", "p");
 
-        str.newLine("Java Academic Program");//2
-
-        str.revers("Coffee");//3
-
-        System.out.println(str.countSubString("Nice to meet you!!", "e"));//4
-
-        System.out.println(str.toUppercase("java"));//5
-
-        System.out.println(str.removeSymbol("java academic program"));//7
-        System.out.println(str.transferSymA("java academic program"));//8
-
-        str.getLongestWord("Hardships often prepare ordinary people for an extraordinary destiny");//9
-
-        str.symbolIndex("oop in java ", "p");
+        System.out.println(str1.firstSymbols("ozymandias"));
+        System.out.println(str1.countXX("dxxx"));
+        System.out.println(str1.palindrome("abcba"));
     }
 }
--- src/string_homework/SecondStringHomework.java
+++ src/string_homework/SecondStringHomework.java
@@ -1,4 +1,63 @@
 package string_homework;
 
 public class SecondStringHomework {
+    /**
+     * Ex.1
+     *
+     * @param word String
+     * @return String
+     */
+    String firstSymbols(String word) {
+        if (word.startsWith("oz")) {
+            return "oz";
+        } else if (word.startsWith("o")) {
+            return "o";
+        } else if (word.startsWith("z")) {
+            return "z";
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Ex.2
+     *
+     * @param line String
+     * @return String
+     */
+    int countXX(String line) {
+        int count = 0;
+        for (int i = 0; i < line.length() - 1; i++) {
+            if (line.substring(i, i + 2).equals("xx")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Ex. 3
+     *
+     * @param line String
+     * @return boolean
+     */
+    boolean palindrome(String line) {
+        String reverseLine = "";
+        int strLength = line.length();
+
+        for (int i = (strLength - 1); i >= 0; --i) {
+            reverseLine = reverseLine + line.charAt(i);
+        }
+
+        if (line.equalsIgnoreCase(reverseLine)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
 }
+
+
