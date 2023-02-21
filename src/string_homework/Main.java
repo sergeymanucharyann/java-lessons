700e84f93020a9da8790a1a542c25725f0bec7b0
Sergey <sergeymanucharyan5@gmail.com>
2023-02-21 13:11:32 -0800

    String Homework

--- src/string_homework/Main.java
+++ src/string_homework/Main.java
@@ -5,23 +5,23 @@ public class Main {
         Strings str = new Strings();
         SecondStringHomework str1 = new SecondStringHomework();
 
-//        System.out.println(str.concat("hello", " world"));
-//        System.out.println(str.concat2(1, 2, " test code"));
-//
-//        str.newLine("Java Academic Program");//2
-//
-//        str.revers("Coffee");//3
-//
-//        System.out.println(str.countSubString("Nice to meet you!!", "e"));//4
-//
-//        System.out.println(str.toUppercase("java"));//5
-//
-//        System.out.println(str.removeSymbol("java academic program"));//7
-//        System.out.println(str.transferSymA("java academic program"));//8
-//
-//        str.getLongestWord("Hardships often prepare ordinary people for an extraordinary destiny");//9
-//
-//        str.symbolIndex("oop in java ", "p");
+        System.out.println(str.concat("hello", " world"));
+        System.out.println(str.concat2(1, 2, " test code"));
+
+        str.newLine("Java Academic Program");//2
+
+        str.revers("Coffee");//3
+
+        System.out.println(str.countSubString("Nice to meet you!!", "e"));//4
+
+        System.out.println(str.toUppercase("java"));//5
+
+        System.out.println(str.removeSymbol("java academic program"));//7
+        System.out.println(str.transferSymA("java academic program"));//8
+
+        str.getLongestWord("Hardships often prepare ordinary people for an extraordinary destiny");//9
+
+        str.symbolIndex("oop in java ", "p");
 
         System.out.println(str1.firstSymbols("ozymandias"));
         System.out.println(str1.countXX("dxxx"));
