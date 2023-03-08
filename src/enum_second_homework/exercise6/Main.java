9bfa4c0c04a70f5dc1b014c30aed68a8510ad69b
Sergey <sergeymanucharyan5@gmail.com>
2023-03-08 01:51:23 -0800

    Add the Main method

---
+++ src/enum_second_homework/exercise6/Main.java
@@ -0,0 +1,8 @@
+package enum_second_homework.exercise6;
+
+public class Main {
+    public static void main(String[] args) {
+        PizzaSize pizzaSize = PizzaSize.SMALL;
+        pizzaSize.pizzaSize();
+    }
+}
