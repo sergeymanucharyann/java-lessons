ddc1e765180d275c2d8f54ce6e0d96f4a5982518
Sergey <sergeymanucharyan5@gmail.com>
2023-03-08 02:10:51 -0800

    - Everything is tested. All methods are worked.

---
+++ src/second_string_homework/Main.java
@@ -0,0 +1,23 @@
+package second_string_homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SecondStringHomework str = new SecondStringHomework();
+
+        System.out.println(str.firstSymbols("ozymandias"));
+
+        System.out.println(str.countXX("dxxx"));
+
+        System.out.println(str.palindrome("abcba"));
+
+        System.out.println(str.toLoweCaseA(new String[]{"asd", "adsfsd", "bas"}));
+
+        System.out.println(str.toLoweCaseA(new String[]{"aaa", "bbb", "acs"}));
+
+        System.out.println(str.commonCharacterCount("Jv", "caeJvmic"));
+
+        System.out.println(str.longestCommonSubstring("java", "javlkll"));
+
+    }
+}
